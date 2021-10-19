06f50e3df795cad44b520e9fb2b87fcb20264c8a
Bart Gijsbertsen <bartgijsbertsen@gmail.com>
2021-10-19 15:12:03 +0200

    Added WxWidgets installation

--- boost_wxwidgets.bash
+++ boost_wxwidgets.bash
@@ -6,8 +6,6 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-START_DIR=$(pwd)
-
 # Boost install
 BOOST_DOWNLOAD_FOLDER=/home/$SUDO_USER/sources/boost_1_77_0
 mkdir $BOOST_DOWNLOAD_FOLDER -p
@@ -21,3 +19,19 @@ bash bootstrap.sh
 sudo ./b2 -j16 toolset=gcc --build-type=complete link=shared runtime-link=shared threading=multi,single variant=debug,release address-model=64 --layout=tagged --without-python --cxxflags="-std=c++17" install stage 2>&1 | tee build.log
 sudo ldconfig
 
+# WxWidgets install
+WX_WIDGETS_FOLDER=/home/$SUDO_USER/sources/wxWidgets_3_1_5
+mkdir $WX_WIDGETS_FOLDER -p
+cd $WX_WIDGETS_FOLDER
+sudo apt install libgtk-3-dev -y
+wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.5/wxWidgets-3.1.5.tar.bz2
+tar -xvjf wxWidgets-3.1.5.tar.bz2
+mv wxWidgets-3.1.5/* .
+mv wxWidgets-3.1.5/.* .
+rmdir wxWidgets-3.1.5
+rm wxWidgets-3.1.5.tar.bz2
+mkdir build-linux -p
+cd build-linux
+../configure --enable-debug --with-cxx=17
+sudo make -j16 install 2>&1 | tee build.log
+sudo ldconfig
